6354a9deffd714dd198c03402eea551d975e94fe
Yves Gallot <59421144+galloty@users.noreply.github.com>
2021-02-23 17:35:35 +0100

    add G(alpha, beta)

--- src/prob.h
+++ src/prob.h
@@ -37,7 +37,7 @@ public:
 		{
 			const double u = 1 + i * dt;
 			f1[i] = 1 - std::log(u); f2[i] = 1;
-			if (i % 4096 == 0) _rho.push_back(Rho(u, f1[i], f2[i]));
+			if (i % step == 0) _rho.push_back(Rho(u, f1[i], f2[i]));
 		}
 
 		double F1 = 2 * n - 1;
@@ -58,7 +58,7 @@ public:
 				f2[i + n] = (f2[i] + 2 * S2 + f1[i] + F1) / (2 * n * u - 1);
 				F1 += 2 * f1[i];
 
-				if (i % 4096 == 0) _rho.push_back(Rho(u, f1[i + n], f2[i + n]));
+				if (i % step == 0) _rho.push_back(Rho(u, f1[i + n], f2[i + n]));
 			}
 			for (size_t i = 0; i < n; ++i) f1[i] = f1[i + n];
 			for (size_t i = 0; i < n; ++i) f2[i] = f2[i + n];
@@ -86,9 +86,56 @@ private:
 		return 0;
 	}
 
+	double F_inv(const double p, const double err, const int k) const
+	{
+		double alpha_min = 0, alpha_max = 1;
+
+		while (true)
+		{
+			const double alpha = 0.5 * (alpha_min + alpha_max);
+			const double p_est = (k == 1) ? F1(alpha) : F2(alpha);
+			if (alpha_max - alpha_min < err * p) return alpha;
+			if (p_est < p) alpha_min = alpha; else alpha_max = alpha;
+		}
+	}
+
 public:
 	double rho1(const double u) const { return rho(u, 1); }
 	double rho2(const double u) const { return rho(u, 2); }
 	double F1(const double alpha) const { return rho(1 / alpha, 1); }
 	double F2(const double alpha) const { return rho(1 / alpha, 2); }
+	double F1_inv(const double p, const double err = 1e-5) const { return F_inv(p, err, 1); }
+	double F2_inv(const double p, const double err = 1e-5) const { return F_inv(p, err, 2); }
+
+	double sigma(const double u, const double v) const
+	{
+		double s = rho1(v);
+		const double a = v + 1 - v / u, b = v;
+		for (int k = 0, n = std::max(1024, int((b - a) * 1024)); k <= n; ++k)
+		{
+			const double dt = (b - a) / double(n), t = a + k * dt;
+			double f = rho1(t - 1) * dt / (v + 1 - t);
+			if ((k == 0) || (k == n)) f *= 0.5;
+			s += f;
+		}
+		return s;
+	}
+
+	double G(const double alpha, const double beta) const { return (beta < alpha) ? sigma(1 / alpha, 1 / beta) : F1(alpha); }
+
+	double B2(const int digits)
+	{
+		const double P = 5 * pow(10.0, digits - 1);
+		double B2_min = 100, B2_max = 1e15;
+		while (true)
+		{
+			const double B2 = std::sqrt(B2_min * B2_max);
+			const double ct_inv = F1(log(B2) / log(P)) * log(B2) / B2;
+			const double B2_p = B2 * 1.0001;
+			const double ct_inv_p = F1(log(B2_p) / log(P)) * log(B2_p) / B2_p;
+			if (ct_inv_p > ct_inv) B2_min = B2; else B2_max = B2;
+			if (fabs(B2_max - B2_min) / B2_max < 1e-6) break;
+		}
+		return 0.5 * (B2_min + B2_max);
+	}
 };
